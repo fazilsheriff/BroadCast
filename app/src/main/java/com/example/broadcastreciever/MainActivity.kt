fcd954ef8aa50e39c74d4072fe2adc1bf65dd743
fazil.s@lantrasoft.com <SinCos123>
2020-10-12 11:09:45 +0530

    Initial commit

--- app/src/main/java/com/example/broadcastreciever/MainActivity.kt
+++ app/src/main/java/com/example/broadcastreciever/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.example.broadcastreciever
 
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //Creating  filters
+        var filter=IntentFilter()
+        // setting filters
+        filter.addAction(Intent.ACTION_POWER_CONNECTED)
+        filter.addAction(Intent.ACTION_POWER_DISCONNECTED)
+        //Receiver Implementation
+        val receiver= object: BroadcastReceiver(){
+            override fun onReceive(context: Context?, intent: Intent?) {
+                Toast.makeText(context,intent?.action,Toast.LENGTH_LONG).show()
+            }
+
+        }
+
+            registerReceiver(receiver,filter)
     }
 }
